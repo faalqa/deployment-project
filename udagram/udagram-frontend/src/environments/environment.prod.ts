ad482d03478047a59cf523e27b5477a49f0b5fb6
Fai <59806790+faalqa@users.noreply.github.com>
2023-01-10 23:50:09 +0300

    frontend linting

--- udagram/udagram-frontend/src/environments/environment.prod.ts
+++ udagram/udagram-frontend/src/environments/environment.prod.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: true,
-  appName: "Udagram",
-  apiHost: "http://udagram-api-dev.eba-uqz4ifua.us-east-1.elasticbeanstalk.com/api/v0",
+  appName: 'Udagram',
+  apiHost: 'http://udagram-api-dev.eba-uqz4ifua.us-east-1.elasticbeanstalk.com/api/v0',
 };
 
 /*
